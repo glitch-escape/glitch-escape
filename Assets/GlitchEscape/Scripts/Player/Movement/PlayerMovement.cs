b5b9561700471599f67f7446007382ffaa765123
Lubin Tu <tlb641226830@gmail.com>
2020-01-08 21:49:43 -0800

    importing works

---
+++ Assets/GlitchEscape/Scripts/Player/Movement/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 10f;
+    public float jumpHeight = 1f;
+
+    private Animator m_Animator;
+    private Rigidbody m_Rigidbody;
+    private Vector3 m_Movement;
+    private Quaternion m_Rotation = Quaternion.identity;
+    private Controls controls;
+    private Vector2 movementInput;
+
+
+    void Awake()
+    {
+        controls = new Controls();
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+    private void OnEnable() => controls.Player.Enable();
+    private void OnDisable() => controls.Player.Disable();
+
+    void FixedUpdate() => Move();
+
+
+    private void Move()
+    {
+        var movementInput = controls.Player.Move.ReadValue<Vector2>();
+
+        float horizontal = movementInput.x;
+        float vertical = movementInput.y;
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+
+    public void Jump()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + jumpHeight, transform.position.z);
+    }
+}
