711634e89346d0165b3b835fdc1ab8e14e7f0214
Lubin Tu <tlb641226830@gmail.com>
2020-01-13 20:45:10 -0800

    finished timer&reset

---
+++ Assets/GlitchEscape/Scripts/UI/HUD.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Text countdownText;
+
+    public float timeLimit = 120f;
+
+    private float counter = 0f;
+    private float timeRemains;
+
+    [System.Obsolete]
+    void Update()
+    {
+        counter += 1 * Time.deltaTime;
+        timeRemains = timeLimit - counter;
+
+        if (timeRemains <= 0)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+        float minRemains = timeRemains / 60;
+        float secRemains = timeRemains % 60;
+
+        countdownText.text = "Time Left: " + minRemains.ToString("0") + ":" + secRemains.ToString("0");
+
+    }
+
+}
+
+
