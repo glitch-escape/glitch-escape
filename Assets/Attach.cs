1c45d55b15f40cf527103d01b292c8f1bc9daaf7
ben861219 <bchen42@ucsc.edu>
2020-01-24 14:34:21 -0800

    Added script to attach player to moving platforms

---
+++ Assets/Attach.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attach : MonoBehaviour
+{
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+    }
+    
+}
