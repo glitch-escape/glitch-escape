43d3bb21bfe6bf44f0ff7daaac6eaf562ccc75d9
Seiji Emery <seijiemery@gmail.com>
2020-02-02 10:18:18 -0800

    fix to Attach script (reset parent)

--- Assets/GlitchEscape/Scripts/Environment/Attach.cs
+++ Assets/GlitchEscape/Scripts/Environment/Attach.cs
@@ -2,24 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attach : MonoBehaviour
-{
-    public GameObject Player;
+public class Attach : MonoBehaviour {
+    private Player player = null;
+    private Transform prevParent = null;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject == Player)
-        {
-            Player.transform.parent = transform;
+    private void OnTriggerEnter(Collider other) {
+        player = other.GetComponent<Player>();
+        if (player != null) {
+            prevParent = player.transform.parent;
+            player.transform.parent = transform;
         }
     }
-    
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject == Player)
-        {
-            Player.transform.parent = null;
+    private void OnTriggerExit(Collider other) {
+        if (player != null) {
+            player.transform.parent = prevParent;
+            prevParent = null;
+            player = null;
         }
     }
-    
 }
