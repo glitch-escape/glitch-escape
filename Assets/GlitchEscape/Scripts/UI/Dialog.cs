85af114757cb5f01bf519560bc188bf07417f07b
Lubin Tu <tlb641226830@gmail.com>
2020-02-02 17:59:04 -0800

    update

--- Assets/GlitchEscape/Scripts/UI/Dialog.cs
+++ Assets/GlitchEscape/Scripts/UI/Dialog.cs
@@ -15,31 +15,43 @@ public class Dialog : MonoBehaviour
     public float charSpeed = 0.03f;
     public float sentenceSpeed = 3;
 
+    private GameObject dialogPannel;
+    private GameObject staticPannel;
+    private GameObject floatPannel;
     private string text = "";
     private int index = 0;
     private int dialogType = 0;
 
     void Start()
     {
+        dialogPannel = dialogText.transform.parent.gameObject;
+        staticPannel = staticText.transform.parent.gameObject;
+        floatPannel = floatText.transform.parent.gameObject;
         StartCoroutine(DisplayByChar());
     }
 
     void Update()
     {
-        dialogText.text = "";
-        staticText.text = "";
-        floatText.text = "";
+        dialogPannel.gameObject.SetActive(false);
+        staticPannel.gameObject.SetActive(false);
+        floatPannel.gameObject.SetActive(false);
+        // dialogText.text = "";
+        // staticText.text = "";
+        // floatText.text = "";
         switch (dialogType)
         {
             case 0:
+                dialogPannel.gameObject.SetActive(true);
                 dialogText.text = text;
                 break;
             case 1:
+                staticPannel.gameObject.SetActive(true);
                 staticText.text = text;
                 break;
             case 2:
+                floatPannel.gameObject.SetActive(true);
                 Vector3 dialogPos = Camera.main.WorldToScreenPoint(floatTextArea.position);
-                floatText.transform.position = dialogPos;
+                floatPannel.transform.position = dialogPos;
                 floatText.text = text;
                 break;
             default:
