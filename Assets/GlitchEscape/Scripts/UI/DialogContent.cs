ad0c896ab1e90339cc6c578a2a8d525a6aa081e5
Lubin Tu <tlb641226830@gmail.com>
2020-02-03 20:23:14 -0800

    update

---
+++ Assets/GlitchEscape/Scripts/UI/DialogContent.cs
@@ -0,0 +1,11 @@
+﻿public class DialogContent
+{
+    public string name;
+    public string sentence;
+
+    public DialogContent(string newName, string newSetence)
+    {
+        name = newName;
+        sentence =  newSetence;
+    }
+}
--- Assets/GlitchEscape/Scripts/UI/NPCInteractTrigger.cs
+++ Assets/GlitchEscape/Scripts/UI/NPCInteractTrigger.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class NPCInteractTrigger : MonoBehaviour
 {
     public Dialog dialogManager;
+    public Transform floatTextArea;
+    public int eventNumber = 0;
+
+    private bool eventTriggered = false;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            dialogManager.changeDialogType(2);
+            dialogManager.CheckPlayerNearBy(true, eventNumber);
+            if (!eventTriggered)
+            {
+                dialogManager.SwitchDialogEvent(eventNumber, floatTextArea);
+                eventTriggered = true;
+            }
         }
     }
     void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player")
         {
-            dialogManager.changeDialogType(1);
+            dialogManager.CheckPlayerNearBy(false, eventNumber);
         }
     }
 }
