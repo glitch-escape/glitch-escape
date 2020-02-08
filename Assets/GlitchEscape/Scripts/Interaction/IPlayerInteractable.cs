a5844594a4f7c1fe991fbcfb78100c926817819a
Seiji Emery <seijiemery@gmail.com>
2020-02-08 04:05:32 -0800

    added interaction system

---
+++ Assets/GlitchEscape/Scripts/Interaction/IPlayerInteractable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPlayerInteractable {
+    void OnInteract(Player player);
+    void OnPlayerEnterInteractionRadius(Player player);
+    void OnPlayerExitInteractionRadius(Player player);
+}
