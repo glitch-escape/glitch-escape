3abd7c3cf0b4366275d1a9baa54732bd5072cdf8
Seiji Emery <seijiemery@gmail.com>
2020-02-08 04:57:09 -0800

    added camera facing script

---
+++ Assets/GlitchEscape/Scripts/Misc/FaceCamera.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour {
+    public enum RotationMode {
+        None,
+        YAxis,
+        XAxis,
+        FullAxes
+    };
+    public RotationMode rotationMode = RotationMode.FullAxes;
+    private Camera camera;
+    void Start() {
+        if (camera == null) { 
+            camera = Camera.main;
+        }
+    }
+    void TurnToFaceCamera() {
+        if (rotationMode == RotationMode.None) return;
+        var toCamera = transform.position - camera.transform.position;
+        switch (rotationMode) {
+            case RotationMode.YAxis: toCamera.y = 0f; break;
+            case RotationMode.XAxis: toCamera.x = 0f; break;
+            case RotationMode.FullAxes: break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        toCamera.Normalize();
+        transform.rotation = Quaternion.LookRotation(toCamera);
+    }
+    void Update() {
+        TurnToFaceCamera();
+    }
+    private void OnEnable() {
+        TurnToFaceCamera();
+    }
+}
