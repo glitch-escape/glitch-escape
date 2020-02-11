02db26a335222e8263e8ae4a3e50fcc4ac1f00e8
Lubin Tu <tlb641226830@gmail.com>
2020-02-10 16:49:36 -0800

    finished

---
+++ Assets/GlitchEscape/Scripts/Interaction/InteractableTank.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(InteractionTrigger))]
+public class InteractableTank : MonoBehaviour, IPlayerInteractable
+{
+    public GameObject playerInTank;
+    public Transform floatTextArea;
+    public FloatingText floatingText;
+    public string interactMessage = "[Extract]";
+
+    public void OnInteract(Player player)
+    {
+        playerInTank.SetActive(false);
+        floatingText.DisableText(floatTextArea);
+    }
+
+    public void OnPlayerEnterInteractionRadius(Player player)
+    {
+        if (playerInTank.activeInHierarchy)
+        {
+            floatingText.EnableText(floatTextArea, interactMessage);
+        }
+    }
+
+    public void OnPlayerExitInteractionRadius(Player player)
+    {
+        floatingText.DisableText(floatTextArea);
+    }
+}
---
+++ Assets/GlitchEscape/Scripts/Misc/FloatingText.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+public class FloatingText : MonoBehaviour
+{
+    private TextMeshProUGUI floatText;
+    private GameObject floatPannel;
+    private Transform floatTextArea;
+
+    void Awake()
+    {
+        // floatText = this.GetComponents<TextMeshProUGUI>();
+        floatText = gameObject.GetComponent<TextMeshProUGUI>();
+        floatPannel = this.transform.parent.gameObject;
+        floatPannel.SetActive(false);
+        floatTextArea = null;
+    }
+
+    void Update()
+    {
+        if (floatTextArea != null)
+        {
+            floatPannel.transform.position = Camera.main.WorldToScreenPoint(floatTextArea.position);
+        }
+    }
+
+    public void EnableText(Transform targetTransform, string text)
+    {
+        floatText.text = text;
+        floatTextArea = targetTransform;
+        floatPannel.SetActive(true);
+    }
+
+    public void DisableText(Transform targetTransform)
+    {
+        if (floatTextArea == targetTransform)
+        {
+            floatTextArea = null;
+            floatPannel.SetActive(false);
+        }
+    }
+}
