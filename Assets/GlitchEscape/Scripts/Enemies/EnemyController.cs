80d1d3eec4128d7f067029d57556775e31c7f1e9
Seiji Emery <seijiemery@gmail.com>
2020-02-12 04:19:21 -0800

    started work on rewriting / rearchitecting the enemy controller
    core idea: enemy controller, break down into behaviors and behavior states,
    which we can attach as monobehaviors + get via GetComponent<> w/ specific
    interfaces; combinatoric use of behavior scripts to implement different
    enemy types.

--- Assets/GlitchEscape/Scripts/Enemies/EnemyController.cs
+++ Assets/GlitchEscape/Scripts/Enemies/EnemyController.cs
@@ -1,14 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 // Interface that all enemy controller subcomponents must implement
 public interface IEnemyControllerComponent {
     void SetupControllerComponent(EnemyController controller);
 }
 
-public class EnemyController : MonoBehaviour {
+public interface IEnemyBehavior {
+    
+}
+public interface IEnemyIdleBehavior : IEnemyBehavior {}
+public interface IEnemyChaseBehavior : IEnemyBehavior {}
+public interface IEnemyAttackBehavior : IEnemyBehavior {}
 
+public interface IEnemySightRadius {
+    bool CanSeePlayer(Enemy agent, Player player);
+}
+
+/// <summary>
+/// All AI behavior states
+/// </summary>
+enum EnemyBehaviorState {
+    /// <summary>
+    /// no current active state - will transition to an Idle state
+    /// </summary>
+    None,
+    
+    /// <summary>
+    /// idle w/ a currently active task (ie. guarding, patrolling, etc)
+    /// </summary>
+    IdleWithTask,
+    
+    /// <summary>
+    /// idle w/out a currently active task - should search for a task to return to
+    /// </summary>
+    IdleUntasked,
+    
+    /// <summary>
+    /// spotted / currently chasing the player
+    /// </summary>
+    ChasingPlayer,
+    
+    /// <summary>
+    /// currently attacking the player
+    /// </summary>
+    AttackingPlayer,
+    
+    /// <summary>
+    /// lost sight of the player - attempt to find the player, or return to a previously active task
+    /// </summary>
+    SearchingForPlayer,
+}
+
+public interface IEnemyBehaviorState {
+    
+}
+
+/// <summary>
+/// An AI controller for an enemy object.
+///
+/// Uses attached IEnemyBehaviors to determine how this agent acts + behaves, and in turn manages + switches between
+/// different IEnemyBehaviorStates that represent specific actions that this agent is executing right now.
+/// </summary>
+public class EnemyController : MonoBehaviour {
+    
     public Enemy enemy;
     public Player player;
 
