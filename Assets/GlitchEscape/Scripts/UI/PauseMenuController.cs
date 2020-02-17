327bb5a618a8549dada93b66be54bcf628a07fac
Lubin Tu <tlb641226830@gmail.com>
2020-02-16 17:37:49 -0800

    finishPauseMenu

--- Assets/GlitchEscape/Scripts/UI/PauseMenuController.cs
+++ Assets/GlitchEscape/Scripts/UI/PauseMenuController.cs
@@ -1,41 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class PauseMenuController : MonoBehaviour
 {
     public GameObject pauseMenu;
-    // input instance singleton
-    // public Input input => m_input ?? (m_input = new Input());
-    // private Input m_input;
-    // private Vector2 uiInput => input.UI.Navigate.ReadValue<Vector2>();
+    public Button resumeButtom;
 
     void Awake()
     {
         pauseMenu.SetActive(false);
-        // input.Enable();
     }
-
-    // void FixedUpdate()
-    // {
-        // if (uiInput.magnitude > 1e-6)
-        // {
-        //     Debug.Log(uiInput.x + uiInput.y);
-        // }
-    // }
 
     public void OnPauseMenu()
     {
-        Time.timeScale = 0;
-        AudioListener.pause = true;
-        pauseMenu.SetActive(true);
+        if (!pauseMenu.activeInHierarchy)
+        {
+            Time.timeScale = 0;
+            AudioListener.pause = true;
+            pauseMenu.SetActive(true);
+            resumeButtom.Select();
+        }
     }
 
     public void GameResume()
     {
-        Time.timeScale = 1;
-        AudioListener.pause = false;
-        pauseMenu.SetActive(false);
+        if (pauseMenu.activeInHierarchy)
+        {
+            Time.timeScale = 1;
+            AudioListener.pause = false;
+            pauseMenu.SetActive(false);
+            EventSystem.current.SetSelectedGameObject(null);
+        }
     }
 }
