199f41033658430ce5ccb9189d113dc2296a4014
Seiji Emery <seijiemery@gmail.com>
2020-02-20 12:29:17 -0800

    refactored to just use c# events
    (note: c# events are more brittle, ie. script references could 
    potentially break during script reloading, but not using c# events for 
    something that they're clearly designed for is a bit silly)

--- Assets/GlitchEscape/Scripts/PlayerAffectorFields/DamageFieldEffect.cs
+++ Assets/GlitchEscape/Scripts/PlayerAffectorFields/DamageFieldEffect.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerAffectorField))]
-public class DamageFieldEffect : MonoBehaviour, IFieldEffect {
+public class DamageFieldEffect : MonoBehaviour {
     public enum DamageEffectType {
         None, OnEnter, ApplyContinuously,
     }
     public DamageEffectType damageType;
     public float damage = 10f;
-    public void SetupField(PlayerAffectorField effectorField) {}
-    public void OnPlayerEnter(Player player) {
-        if (damageType == DamageEffectType.OnEnter) {
-            player.TakeDamage(damage);
-        }
-    }
-    public void OnPlayerExit(Player player) {}
-    public void OnPlayerTick(Player player) {
-        if (damageType == DamageEffectType.ApplyContinuously) {
-            player.TakeDamage(damage * Time.deltaTime);
-        }
+
+    void Awake() {
+        var affectorField = Enforcements.GetComponent<PlayerAffectorField>(this);
+        affectorField.OnPlayerEnter += player => {
+            if (damageType == DamageEffectType.OnEnter) {
+                player.TakeDamage(damage);
+            }
+        };
+        affectorField.OnPlayerTick += player => {
+            if (damageType == DamageEffectType.OnEnter) {
+                player.TakeDamage(damage);
+            }
+        };
     }
 }
--- Assets/GlitchEscape/Scripts/PlayerAffectorFields/PlayerAffectorField.cs
+++ Assets/GlitchEscape/Scripts/PlayerAffectorFields/PlayerAffectorField.cs
@@ -6,18 +6,48 @@ using UnityEngine;
 /// <summary>
 /// Creates a collision or trigger based field that can affect the player in various ways.
 ///
-/// Effects are implemented using IFieldEffects
-/// (see DamageFieldEffect, ReduceStaminaFieldEffect, ReduceMaxHealthFieldEffect, KnockbackEffect)
+/// Effects are implemented using field effects:
+/// (DamageFieldEffect, ReduceStaminaFieldEffect, ReduceMaxHealthFieldEffect, KnockbackEffect, etc)
 /// </summary>
 [RequireComponent(typeof(Collider))]
 public class PlayerAffectorField : MonoBehaviour {
+    
+    #region PublicInterface
+    public delegate void FieldEvent(Player player);
+
+    /// <summary>
+    /// Called when player enters the field
+    /// </summary>
+    public FieldEvent OnPlayerEnter;
+    
+    /// <summary>
+    /// Called when player exits the field
+    /// </summary>
+    public FieldEvent OnPlayerExit;
+    
+    /// <summary>
+    /// Called for each frame when player is currently in the field
+    /// </summary>
+    public FieldEvent OnPlayerTick;
+
+    /// <summary>
+    /// Can use this to set the current trigger type while hiding the actual collider reference
+    /// </summary>
+    public bool isTrigger {
+        get => collider.isTrigger;
+        set => collider.isTrigger = value;
+    }
     
     /// <summary>
     /// active player, if they're currently inside of this field
     /// </summary>
     [HideInInspector]
     public Player activePlayer { get; private set; } = null;
-
+    
+    #endregion
+    
+    #region InspectorProperties
+    
     /// <summary>
     /// collision type - explicitely manages switching between triggers + collision based effects
     /// could be extended to support other collision types
@@ -26,55 +56,23 @@ public class PlayerAffectorField : MonoBehaviour {
     };
     private CollisionType collisionType;
     
-    /// <summary>
-    /// hide direct collider reference but let scripts explicitely set trigger type, if necessary, through this
-    /// </summary>
-    public bool isTrigger {
-        get { return collisionType == CollisionType.Trigger; }
-        set {
-            collisionType = value ? CollisionType.Trigger : CollisionType.Collider;
-            collider.isTrigger = value;
-        }
-    }
+    #endregion
     
+    #region ImplementationDetails
     /// <summary>
     /// collider reference - hidden from other scripts
     /// </summary>
     private Collider collider => _collider ?? Enforcements.GetComponent(this, out _collider);
     private Collider _collider = null;
-    
-    /// <summary>
-    /// array of field effect impls - hidden from other scripts
-    /// </summary>
-    private IFieldEffect[] effects => _effects ?? Enforcements.GetComponents(this, out _effects);
-    private IFieldEffect[] _effects = null;
 
     void Awake() {
         // make sure our collider reference exists, and explicitely set the trigger type so it matches if necessary
         var collider = this.collider;
         if (collider.isTrigger != isTrigger) collider.isTrigger = isTrigger;
-        
-        // call SetupFieldBarrier() on all scripts
-        foreach (var effect in effects) {
-            effect.SetupField(this);
-        }
-    }
-
-    private void OnPlayerExit(Player player) {
-        foreach (var effect in effects) {
-            effect.OnPlayerExit(player);
-        }
-    }
-    private void OnPlayerEnter(Player player) {
-        foreach (var effect in effects) {
-            effect.OnPlayerEnter(player);
-        }
     }
     private void Update() {
         if (activePlayer != null) {
-            foreach (var effect in effects) {
-                effect.OnPlayerTick(activePlayer);
-            }
+            OnPlayerTick(activePlayer);
         }
     }
     private void OnTriggerEnter(Collider other) {
@@ -113,4 +111,5 @@ public class PlayerAffectorField : MonoBehaviour {
             }
         }
     }
+    #endregion
 }
