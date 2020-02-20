f25c820e39f86973635fd81c686f61748cb1993b
Seiji Emery <seijiemery@gmail.com>
2020-02-20 11:11:21 -0800

    started writing field barrier script

---
+++ Assets/GlitchEscape/Scripts/FieldBarrier/FieldBarrier.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldBarrier : MonoBehaviour {
+    public enum CollisionType {
+        Trigger,
+        Collider
+    };
+    public CollisionType collisionType;
+    public Player activePlayer { get; private set; } = null;
+    public Collider collider => _collider ?? (_collider = GetComponent<Collider>()) 
+                                ?? (Debug.LogError("FieldBarrier.cs: missing collider!"), null));
+    private IFieldBarrierEffect[] effects => _effects ?? (_effects = GetComponents<IFieldBarrierEffect>());
+    private Collider _collider = null;
+    private IFieldBarrierEffect[] _effects = null;
+
+    
+    
+    
+    
+    void Awake() {
+        effects = GetComponents<IFieldBarrierEffect>();
+    }
+    
+    
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/GlitchEscape/Scripts/FieldBarrier/IFieldBarrierEffect.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IFieldBarrierEffect {
+    void SetupFieldBarrier(FieldBarrier barrier);
+    void OnPlayerEnter(Player player);
+    void OnPlayerExit(Player player);
+    void OnPlayerTick(Player player);
+}
