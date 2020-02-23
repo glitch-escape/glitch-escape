34a0eb80afc9475f0764cd9eaf151b8e092cd565
Seiji Emery <seijiemery@gmail.com>
2020-02-23 00:01:04 -0800

    add level restart via 'R' button

---
+++ Assets/GlitchEscape/Scripts/All/RestartLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.rKey.isPressed) {
+            SceneManager.LoadScene(0);
+        }       
+    }
+}
