f3a9cee1813b30c7a127e68a61e293a39cc97066
Lubin Tu <tlb641226830@gmail.com>
2020-02-29 01:05:07 -0800

    add some menus

---
+++ Assets/GlitchEscape/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public GameObject menus;
+    public GameObject main;
+    public GameObject keybinds;
+
+    public Button mainResumeButtom;
+    public Button returnButtom;
+
+    void Awake()
+    {
+        ResetMenu();
+    }
+
+    public void OnPauseMenu()
+    {
+        if (!menus.activeInHierarchy)
+        {
+            Time.timeScale = 0;
+            AudioListener.pause = true;
+            menus.SetActive(true);
+            mainResumeButtom.Select();
+        }
+        else
+        {
+            GameResume();
+        }
+    }
+
+    // basic navigating for different Menus 
+    public void MenuNav(Button seletedButton)
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        seletedButton.Select();
+    }
+
+    public void MenuNavFrom(GameObject navFrom)
+    {
+        navFrom.SetActive(false);
+    }
+
+    public void MenuNavTo(GameObject navTo)
+    {
+        navTo.SetActive(true);
+    }
+
+    public void GameResume()
+    {
+        if (menus.activeInHierarchy)
+        {
+            Time.timeScale = 1;
+            AudioListener.pause = false;
+            menus.SetActive(false);
+            EventSystem.current.SetSelectedGameObject(null);
+            ResetMenu();
+        }
+    }
+
+    public void GameRestart()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+        GameResume();
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+
+    void ResetMenu()
+    {
+        foreach (Transform child in menus.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        menus.SetActive(false);
+        main.SetActive(true);
+    }
+}
