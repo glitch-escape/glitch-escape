67c3f981337b6b062766df36f5770cf906af9664
Lubin Tu <tlb641226830@gmail.com>
2020-02-29 22:02:03 -0800

    added main menu

---
+++ Assets/GlitchEscape/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Scene scene;
+
+    public Button DefaultButtom;
+
+    void Start()
+    {
+        DefaultButtom.Select();
+    }
+
+    // basic navigating for different Menus 
+    public void MenuNav(Button seletedButton)
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        seletedButton.Select();
+    }
+
+    public void MenuNavFrom(GameObject navFrom)
+    {
+        navFrom.SetActive(false);
+    }
+
+    public void MenuNavTo(GameObject navTo)
+    {
+        navTo.SetActive(true);
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+
+
+}
