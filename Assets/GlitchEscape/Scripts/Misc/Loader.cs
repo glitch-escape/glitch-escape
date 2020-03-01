b9aac0cbe489c671d6474d7a263eaf6b3cbba61f
Lubin Tu <tlb641226830@gmail.com>
2020-03-01 00:28:02 -0800

    Add loader

---
+++ Assets/GlitchEscape/Scripts/Misc/Loader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader
+{
+    private class LoadingMonoBehavior : MonoBehaviour { }
+
+    public enum Scene
+    {
+        Loading, MainMenu, Opening, Level1
+    }
+
+    private static Action onLoadCallback;
+    private static AsyncOperation loadingAsyncOperation;
+
+    public static void Load(Scene scene)
+    {
+        onLoadCallback = () =>
+        {
+            GameObject loadingGameObject = new GameObject("Loading Game Object");
+            loadingGameObject.AddComponent<LoadingMonoBehavior>().StartCoroutine(LoadSceneAsync(scene));
+        };
+
+
+        SceneManager.LoadScene(Scene.Loading.ToString());
+    }
+
+    private static IEnumerator LoadSceneAsync(Scene scene)
+    {
+        yield return null;
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(scene.ToString());
+        while (!asyncOperation.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public static float GetLoadingProgress()
+    {
+        if (loadingAsyncOperation != null)
+        {
+            return loadingAsyncOperation.progress;
+        }
+        else
+        {
+            return 1f;
+        }
+    }
+
+    public static void LoaderCallback()
+    {
+        if (onLoadCallback != null)
+        {
+            onLoadCallback();
+            onLoadCallback = null;
+        }
+    }
+}
---
+++ Assets/GlitchEscape/Scripts/UI/OpeningController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class OpeningController : MonoBehaviour
+{
+    public Scene scene;
+
+    public Button defaultButtom;
+
+    public List<GameObject> ops;
+
+    private int counter = 1;
+
+    void Start()
+    {
+        foreach (GameObject op in ops)
+        {
+            op.SetActive(false);
+        }
+        defaultButtom.Select();
+        ops[0].SetActive(true);
+    }
+
+    // basic navigating for different Menus 
+    public void OnContinue()
+    {
+        if (counter >= ops.Count)
+        {
+            GameStart();
+        }
+        else
+        {
+            ops[counter - 1].SetActive(false);
+            ops[counter].SetActive(true);
+            counter++;
+        }
+
+    }
+
+    public void GameStart()
+    {
+        Loader.Load(Loader.Scene.Level1);
+    }
+}
