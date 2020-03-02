e1195819bd1de4ccb27aeaecb9e2aa433765060f
Seiji Emery <seijiemery@gmail.com>
2020-03-01 16:58:49 -0800

    add test scene + fix input (note: must call Enable() before using)

---
+++ Assets/GlitchEscape/Scripts/Test/PlayerControlsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerControls))]
+public class PlayerControlsTest : MonoBehaviour {
+    
+    private string getActiveControlSchemeLabel () {
+        switch (PlayerControls.activeControlType) {
+            case PlayerControls.InputControlType.None: return "None";
+            case PlayerControls.InputControlType.DualshockGamepad: return "DS4 Gamepad";
+            case PlayerControls.InputControlType.XboxGamepad: return "Xbox Gamepad";
+            case PlayerControls.InputControlType.MouseAndKeyboard: return "Mouse + Keyboard";
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+    private void OnGUI() {
+        GUILayout.Label("Active player controls: " + PlayerControls.instance + " on object " 
+                        + PlayerControls.instance.gameObject);
+        GUILayout.Label("Active control scheme: " + getActiveControlSchemeLabel());
+        GUILayout.Label("Look input: "+PlayerControls.instance.lookInput);
+        GUILayout.Label("Move input: "+PlayerControls.instance.moveInput);
+    }
+}
