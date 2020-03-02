57b95174e2900221c1ef29fec75952c4520ecb0b
Hardwin Bui <buihardwin@yahoo.com>
2020-03-01 18:31:27 -0800

    radial enemy complete

--- Assets/GlitchEscape/Scripts/Enemies/EnemyShoot.cs
+++ Assets/GlitchEscape/Scripts/Enemies/EnemyShoot.cs
@@ -9,6 +9,7 @@ public class EnemyShoot : MonoBehaviour, IEnemyAttackAction {
     private Player player;
 
     // Attack variables
+    public float duration;
     public float cooldown, strikeDist;
     public float projYShift;
     public Projectile bullPrefab;
@@ -18,6 +19,8 @@ public class EnemyShoot : MonoBehaviour, IEnemyAttackAction {
     // Other variables
     private float curAtkTime, curCooldwn;
     private int shotsMade;
+    private bool isAttack =>
+        curAtkTime < duration && curAtkTime > 0;
 
     // Initialize component
     public void SetupControllerComponent(EnemyController controller) {
@@ -47,12 +50,15 @@ public class EnemyShoot : MonoBehaviour, IEnemyAttackAction {
     // Update variables of the attack
     public void UpdateAction() {
         Vector3 dir = player.transform.position - enemy.transform.position;
-        ShootBullet(dir, enemy.transform.position);
+        curAtkTime += Time.deltaTime;
+        if (shotsMade < bulletAmt) {
+            ShootBullet(dir, enemy.transform.position);
+        }
     }
 
     // Informs if the attack has completed
     public bool ActionFinished(out EnemyBehaviorState nextAction) {
-        if (shotsMade >= bulletAmt) {
+        if (!isAttack && shotsMade >= bulletAmt) {
             nextAction = EnemyBehaviorState.ChasingPlayer;
             EndAction();
             return true;
@@ -77,12 +83,16 @@ public class EnemyShoot : MonoBehaviour, IEnemyAttackAction {
     #endregion
 
     #region OtherFunctions
+    // Remove cooldown on enable
+    void OnEnable() {
+        curCooldwn = -cooldown;
+    }
+
     // Update the cooldown tracker
     public void UpdateCooldown() { curCooldwn -= Time.deltaTime; }
 
     // Returns true when attack is over
     public void ShootBullet(Vector3 direction, Vector3 origin) {
-        curAtkTime += Time.deltaTime;
         if (shotsMade * bulletRate < curAtkTime) {
             shotsMade += 1;
 
