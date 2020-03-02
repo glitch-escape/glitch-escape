85c708f6f11fc9f324606529d0624f46855c0474
Hardwin Bui <buihardwin@yahoo.com>
2020-03-01 16:19:50 -0800

    Projectile now uses rigidbody

--- Assets/GlitchEscape/Scripts/Enemies/Projectile.cs
+++ Assets/GlitchEscape/Scripts/Enemies/Projectile.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Projectile : MonoBehaviour {
 
-    private Vector3 direction;
+     private Vector3 direction;
+    public float acceleration;
     public float speed, damage;
     public float lifetime;
 
     void Awake() {
         Destroy(gameObject, lifetime);
+        GetComponent<Rigidbody>().velocity = Vector3.forward * speed;
     }
 
     void Update() {
-        transform.Translate(direction.x * speed, 0, direction.z * speed);
+        GetComponent<Rigidbody>().AddForce(Vector3.forward * acceleration);
+
     }
 
     void OnTriggerEnter(Collider other) {
@@ -21,7 +25,9 @@ public class Projectile : MonoBehaviour {
         if (player != null) { player.TakeDamage(damage); }
     }
 
+    
     public void SetDirection(Vector3 dir) {
         direction = dir;
     }
+    
 }
