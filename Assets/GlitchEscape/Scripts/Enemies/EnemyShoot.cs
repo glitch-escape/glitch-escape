3cf0b93903a75b4492028f25a78b02504bb4cf63
Hardwin Bui <buihardwin@yahoo.com>
2020-03-01 18:52:57 -0800

    completed stationary canon enemy

--- Assets/GlitchEscape/Scripts/Enemies/EnemyShoot.cs
+++ Assets/GlitchEscape/Scripts/Enemies/EnemyShoot.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider))]
 public class EnemyShoot : MonoBehaviour, IEnemyAttackAction {
     private Enemy enemy;
     private EnemyController enemyController;
