6a807dd7fb8c7bca470b7f9d86d42ff63167be9a
Seiji Emery <seijiemery@gmail.com>
2020-03-05 00:21:42 -0800

    added RegeneratingResource + BaseAgent

---
+++ Assets/GlitchEscape/Scripts/GenericAgentsAndEffects/BaseAgent.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Base agent type used as the foundation for Player + Enemy types.
+///
+/// Implements IConfigurable, ie. ScriptableObject config data associated with this player / enemy, accessible from
+/// any reference to this class, and used to 
+/// 
+/// </summary>
+/// <typeparam name="Derived">class that is inheiriting from BaseAgent</typeparam>
+/// <typeparam name="Config">Config data class</typeparam>
+/// <typeparam name="Health">Health class</typeparam>
+/// <typeparam name="Stamina">Stamina class</typeparam>
+public abstract class BaseAgent<Derived, Config, Health, Stamina> : MonoBehaviour, IConfigurable<Config>
+    where Health : Resource<Derived, Config, float>
+    where Stamina : Resource<Derived, Config, float>
+    where Derived : class, IConfigurable<Config>
+    where Config : ScriptableObject
+{
+    [SerializeField]
+    public Config _config;
+    public Config config {
+        get { return _config; }
+        set { _config = value; }
+    }
+
+    public Health health => this.GetEnforcedComponentReference(ref _health);
+    private Health _health = null;
+
+    public Stamina stamina => this.GetEnforcedComponentReference(ref _stamina);
+    private Stamina _stamina = null;
+    
+    public delegate void Listener();
+    public event Listener OnFailedToUseAbility;
+    public event Listener OnKilled;
+
+    protected void OnEnable() { health.OnDepleted += Kill; }
+    protected void OnDisable() { health.OnDepleted -= Kill; }
+
+    /// <summary>
+    /// Take damage
+    /// </summary>
+    public void TakeDamage(float damage) {
+        health.value -= damage;
+    }
+    
+    /// <summary>
+    /// Immediately kill + destroy this agent.
+    /// May fail (eg. Player, which will use TryKillAgent() to just respawn instead)
+    /// </summary>
+    public void Kill() {
+        if (TryKillAgent()) {
+            OnKilled?.Invoke();
+            Destroy(gameObject);
+        }
+    }
+    protected abstract bool TryKillAgent();
+    
+    public bool TryUseAbility(float cost) {
+        if (stamina.value < cost) {
+            OnFailedToUseAbility?.Invoke();
+            return false;
+        }
+        stamina.value -= cost;
+        return true;
+    }
+}
---
+++ Assets/GlitchEscape/Scripts/GenericAgentsAndEffects/RegeneratingResource.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+
+public abstract class RegeneratingResource<Owner, Config> : Resource<Owner, Config, float> 
+    where Owner : class, IConfigurable<Config>
+    where Config : ScriptableObject
+{
+    /// <summary>
+    /// Is regeneration currently enabled?
+    /// </summary>
+    public abstract bool regenEnabled { get; }
+    
+    /// <summary>
+    /// Regen rate, per second
+    /// </summary>
+    public abstract float regenPerSec { get; }
+    
+    /// <summary>
+    /// Delay before regen starts, in seconds
+    /// </summary>
+    public abstract float regenDelay { get; }
+    
+    /// <summary>
+    /// Animation curve for regen over time. May return null, in which case regen is linear.
+    /// </summary>
+    public abstract AnimationCurve regenCurve { get; }
+
+    /// <summary>
+    /// called when regeneration starts
+    /// </summary>
+    public event Listener onRegenStarted;
+
+    /// <summary>
+    /// called when regen ends
+    /// </summary>
+    public event Listener onRegenEnded;
+
+    /// <summary>
+    /// is this resource currently regenerating?
+    /// </summary>
+    public bool regenActive => regenEnabled &&
+                               (regenPerSec >= 0f
+                                   ? value < maximum
+                                   : value > minimum);
+    private bool wasActiveLastFrame = false;
+    private float regenStartTime = 0f;
+
+    /// <summary>
+    /// current regen value
+    /// </summary>
+    public float currentRegenValue => EvaluateRegenAt(Time.time - regenStartTime - regenDelay);
+
+    float EvaluateRegenAt(float time) {
+        if (time < 0f) return 0f;
+        
+        // TODO: apply animation curve
+        
+        return regenPerSec;
+    }
+    
+    public new void Reset() {
+        wasActiveLastFrame = false;
+        regenStartTime = Time.time;
+        base.Reset();
+    }
+
+    public void Update() {
+        // check regen state + fire off event listeners
+        var active = regenActive;
+        if (active != wasActiveLastFrame) {
+            wasActiveLastFrame = active;
+            if (active) onRegenStarted?.Invoke();
+            else onRegenEnded?.Invoke();
+        }
+        
+        // update resource using current regen if active
+        if (active) {
+            value = value + currentRegenValue * Time.deltaTime;
+        }
+    }
+}
