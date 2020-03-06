5392cbc4bda297a1b0ceaa084ca7b246f7a24b32
Seiji Emery <seijiemery@gmail.com>
2020-03-05 16:44:07 -0800

    now able to accurately determine if a field is a unity component, has a custom field attribute (eg. [Injectable]), and is null

--- Assets/GlitchEscape/Scripts/Utility/Autowire.cs
+++ Assets/GlitchEscape/Scripts/Utility/Autowire.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 
 public class Autowire : MonoBehaviour {
+
+    private static bool IsValidAutowireTarget(FieldInfo field) {
+        var attribs = field.GetCustomAttributes(true);
+        foreach (var attrib in attribs) {
+            if (attrib is InjectComponent) return true;
+        }
+        return false;
+    }
+    private static bool IsUnityComponent(Type type) {
+        var componentType = typeof(Component);
+        while (type != null) {
+            if (type == componentType) return true;
+            type = type.BaseType;
+        }
+        return false;
+    }
 
     public string info;
     public void Run() {
@@ -18,15 +35,15 @@ public class Autowire : MonoBehaviour {
             var fields = type.GetFields();
             foreach (var field in fields) {
                 info += "field " + field + "\n";
+                info += "field type: " + field.FieldType + "\n";
+                info += "is unity component? " + IsUnityComponent(field.FieldType) + "\n";
+                info += "is injectable? " + IsValidAutowireTarget(field) + "\n";
                 var attribs = field.GetCustomAttributes(true);
                 foreach (var attrib in attribs) {
                     info += "field attrib: " + attrib + "\n";
                 }
                 info += "field value " + field.GetValue(component);
             }
-            // foreach (var attrib in attribs) {
-            //     info += "" + attrib + "\n";
-            // }
         }
     }
 }
