91fe9875d656fbc06fb89ee2004740314a548345
Seiji Emery <seijiemery@gmail.com>
2020-04-22 17:38:54 -0700

    updated tests (WIP)

--- Assets/GlitchEscape/Scripts/Tests/EffectSystemTest.cs
+++ Assets/GlitchEscape/Scripts/Tests/EffectSystemTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
@@ -37,36 +38,73 @@ namespace Tests
                     Debug.Log("Increased test count to "+state.testEffectCount);
                 }
             }
-            public IEffect IncreaseTestCount () {
-                return state.CreateEffect(new IncreaseTestCountEffect());
-            }
             struct SetFooEffect : IEffector<EffectTest, State> {
                 public string value;
                 public void Apply(State state) { ++state.testEffectCount; state.foo = value; }
-            }
-            public IEffect SetFoo(string value) {
-                return state.CreateEffect(new SetFooEffect { value = value });
             }
             struct SetBarEffect : IEffector<EffectTest, State> {
                 public int value;
                 public void Apply(State state) { ++state.testEffectCount; state.bar = value; }
             }
-            public IEffect SetBar(int value) {
-                return state.CreateEffect(new SetBarEffect { value = value });
-            }
             struct AddBarEffect : IEffector<EffectTest, State> {
                 public int value;
                 public void Apply(State state) { ++state.testEffectCount; state.bar += value; }
             }
+            public IEffect IncreaseTestCount () {
+                return state.CreateEffect(new IncreaseTestCountEffect());
+            }
+            public IEffect SetFoo(string value) {
+                return state.CreateEffect(new SetFooEffect { value = value });
+            }
+            public IEffect SetBar(int value) {
+                return state.CreateEffect(new SetBarEffect { value = value });
+            }
             public IEffect AddBar(int value) {
                 return state.CreateEffect(new AddBarEffect { value = value });
             }
-            struct MulBarEffect : IEffector<EffectTest, State> {
-                public int value;
-                public void Apply(State state) { ++state.testEffectCount; state.bar *= value; }
+        }
+        
+        class MockEffectBehavior : IEffectBehavior {
+            public bool wasActiveSetTrue = false;
+            public bool wasActiveSetFalse  = false;
+            public bool wasActiveSet => wasActiveSetTrue || wasActiveSetFalse;
+            public bool wasCancelled = false;
+            public bool wasReset = false;
+            public int updateCount = 0;
+            public int operationCount = 0;
+            public bool isActive   { get; set; }
+            public bool isFinished { get; set; }
+            public bool defaultValueOnReset { get; set; } = false;
+
+            public MockEffectBehavior(bool active, bool finished = false) {
+                isActive = active;
+                isFinished = finished;
+            }
+            public bool active {
+                get => isActive;
+                set {
+                    if (value) wasActiveSetTrue = true;
+                    else wasActiveSetFalse = true;
+                    ++operationCount;
+                    isActive = value;
+                }
+            }
+            public bool finished => isFinished;
+            public void OnCancelled() {
+                wasCancelled = true;
+                ++operationCount;
+            }
+            public void Reset() {
+                wasReset = true;
+                isActive = defaultValueOnReset;
+                ++operationCount;
+            }
+            public void Update() {
+                ++updateCount;
+                ++operationCount;
             }
         }
-
+        
         [Test]
         public void TestEffectStateInitialization() {
             var component = new EffectTest();
@@ -84,6 +122,160 @@ namespace Tests
             Assert.AreEqual("", component.state.foo);
             Assert.AreEqual((double)component.config.bar, (double)component.state.bar);
         }
+
+        [Test]
+        public void TestNoEffectBehaviorFunctionsAreCalledNewEffect() {
+            var test = new EffectTest();
+            var behavior = new MockEffectBehavior(true, false);
+            var effect = test.SetFoo("fubar").WithCustomBehavior(behavior);
+            Assert.AreEqual(0, behavior.operationCount);
+            Assert.IsFalse(behavior.wasActiveSet);
+            Assert.IsFalse(behavior.wasCancelled);
+            Assert.IsFalse(behavior.wasReset);
+            Assert.AreEqual(0, behavior.updateCount);
+        }
+        
+        [Test]
+        public void TestEffectBehaviorThatStartsInactive() {
+            TestEffectBehavior(false, false);
+        }
+        
+        [Test]
+        public void TestEffectBehaviorThatStartsActive() {
+            TestEffectBehavior(true, false);
+        }
+        
+        [Test]
+        public void TestEffectBehaviorThatStartsFinishedAndInactive() {
+            TestEffectBehavior(false, true);
+        }
+        
+        [Test]
+        public void TestEffectBehaviorThatStartsFinishedAndActive() {
+            TestEffectBehavior(false, true);
+        }
+        private void TestEffectBehavior(bool startsActive, bool startsFinished) {
+            var test = new EffectTest();
+            test.Init();
+            var behavior = new MockEffectBehavior(startsActive, startsFinished);
+            var effect = test.SetFoo("fubar").WithCustomBehavior(behavior);
+
+            var expectActive = startsActive;
+            var expectFinished = startsFinished;
+
+            // check initial values
+            Assert.AreEqual(0, behavior.operationCount);
+            Assert.AreEqual(false, behavior.wasActiveSet);
+            Assert.AreEqual(false, behavior.wasCancelled);
+            Assert.AreEqual(false, behavior.wasReset);
+            Assert.AreEqual(0, behavior.updateCount);
+            Assert.AreEqual(expectFinished, effect.finished);
+            Assert.AreEqual(expectActive, effect.active);
+
+            // test set active = true
+            effect.active = true;
+            expectActive = true && !expectFinished;
+            Assert.AreEqual(expectActive, effect.active);
+            Assert.AreEqual(expectFinished, effect.finished);
+            Assert.AreEqual(1, behavior.operationCount);
+            Assert.AreEqual(true, behavior.wasActiveSetTrue);
+            Assert.AreEqual(false, behavior.wasActiveSetFalse);
+            behavior.wasActiveSetTrue = false;
+            
+            // test set active = false
+            effect.active = false;
+            expectActive = false;
+            Assert.AreEqual(expectActive, effect.active);
+            Assert.AreEqual(expectFinished, effect.finished);
+            Assert.AreEqual(2, behavior.operationCount);
+            Assert.AreEqual(false, behavior.wasActiveSetTrue);
+            Assert.AreEqual(true, behavior.wasActiveSetFalse);
+            behavior.wasActiveSetFalse = false;
+            Assert.AreEqual(false, behavior.wasActiveSet);
+            
+            // test Cancel()
+            effect.Cancel();
+            Assert.AreEqual(3, behavior.operationCount);
+            Assert.AreEqual(true, behavior.wasCancelled);
+            Assert.AreEqual(false, behavior.wasActiveSet);
+            Assert.AreEqual(false, effect.active);
+            Assert.AreEqual(true, effect.finished);
+            
+            // test Cancel() again
+            effect.Cancel();
+            Assert.AreEqual(3, behavior.operationCount);
+            Assert.AreEqual(false, behavior.wasCancelled);
+            Assert.AreEqual(false, behavior.wasActiveSet);
+            Assert.AreEqual(false, effect.active);
+            Assert.AreEqual(true, effect.finished);
+            
+            // test Start() (should do nothing since finished)
+            effect.Start();
+            Assert.AreEqual(3, behavior.operationCount);
+            Assert.AreEqual(false, behavior.wasActiveSet);
+            
+            // test Reset() w/ reset-to-active true
+            effect.Reset();
+            behavior.defaultValueOnReset = true;
+            Assert.AreEqual(4, behavior.operationCount);
+            Assert.AreEqual(true, behavior.wasReset);
+            Assert.AreEqual(behavior.defaultValueOnReset, effect.active);
+            Assert.AreEqual(false, effect.finished);
+            behavior.wasReset = false;
+            
+            // test Reset() w/ reset-to-active false
+            effect.Reset();
+            behavior.defaultValueOnReset = false;
+            Assert.AreEqual(5, behavior.operationCount);
+            Assert.AreEqual(true, behavior.wasReset);
+            Assert.AreEqual(behavior.defaultValueOnReset, effect.active);
+            Assert.AreEqual(false, effect.finished);
+            behavior.wasReset = false;
+
+            // test Start() (when not finished; should start effect)
+            behavior.wasActiveSetTrue = behavior.wasActiveSetFalse = false;
+            Assert.AreEqual(false, behavior.wasActiveSet);
+            effect.Start();
+            Assert.AreEqual(6, behavior.operationCount);
+            Assert.AreEqual(true, behavior.wasActiveSet);
+            Assert.AreEqual(true, behavior.wasActiveSetTrue);
+            Assert.AreEqual(false, behavior.wasActiveSetFalse);
+            Assert.AreEqual(true, effect.active);
+            Assert.AreEqual(false, effect.finished);
+            
+            // test Start() again (should have no effect)
+            behavior.wasActiveSetTrue = behavior.wasActiveSetFalse = false;
+            Assert.AreEqual(false, behavior.wasActiveSet);
+            effect.Start();
+            Assert.AreEqual(7, behavior.operationCount);
+            Assert.AreEqual(false, behavior.wasActiveSet);
+            Assert.AreEqual(true, effect.active);
+            Assert.AreEqual(false, effect.finished);
+
+            // test Restart() w/ reset-to-active true (while started)
+            behavior.defaultValueOnReset = true;
+            behavior.wasReset = false;
+            behavior.wasActiveSetTrue = behavior.wasActiveSetFalse = false;
+            effect.Restart();
+            Assert.AreEqual(8, behavior.operationCount);
+            Assert.AreEqual(true, behavior.wasReset);
+            Assert.AreEqual(false, behavior.wasActiveSet);
+            Assert.AreEqual(behavior.defaultValueOnReset, effect.active);
+            Assert.AreEqual(false, effect.finished);
+            
+            // test Restart() w/ reset-to-active false (while started)
+            behavior.defaultValueOnReset = true;
+            behavior.wasReset = false;
+            behavior.wasActiveSetTrue = behavior.wasActiveSetFalse = false;
+            effect.Restart();
+            Assert.AreEqual(8, behavior.operationCount);
+            Assert.AreEqual(true, behavior.wasReset);
+            Assert.AreEqual(false, behavior.wasActiveSet);
+            Assert.AreEqual(behavior.defaultValueOnReset, effect.active);
+            Assert.AreEqual(false, effect.finished);
+        }
+        
+        
 
         [Test]
         public void TestEffectApplication() {
@@ -269,7 +461,7 @@ namespace Tests
             Assert.AreEqual(true, effect.finished);
             Assert.AreEqual(0, (double)component.state.testEffectCount);
         }
-
+        
         [Test]
         public void TestEffectsAreNotCancelledWhenSetInactive() {
             var component = new EffectTest();
