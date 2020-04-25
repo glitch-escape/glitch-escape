a5a47672046a458f48830e9744004a6edb6bcc0c
Seiji Emery <seijiemery@gmail.com>
2020-04-24 22:59:08 -0700

    fragment shader + visual test

---
+++ Assets/GlitchEscape/Scripts/Mechanics/Fragments/Fragment.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fragment : MonoBehaviour {
+    void Start() {
+        // randomize fragment vfx
+        GetComponent<Renderer>().material.SetFloat("Seed", Random.Range(-1000f, 0f));
+    }
+}
