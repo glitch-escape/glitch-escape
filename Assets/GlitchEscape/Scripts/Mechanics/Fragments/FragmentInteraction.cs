c832f232d37099dcb9d815a21ebdca6fd6ec82ff
KrutoyChuvak <vkardash@ucsc.edu>
2020-04-27 20:02:02 -0700

    super basic implementation of the fragment pickups
    added test level for fragment pickups and a quick class that lets you pick up fragments based off of polysphere interation code

---
+++ Assets/GlitchEscape/Scripts/Mechanics/Fragments/FragmentInteraction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InteractionTrigger))]
+public class FragmentInteraction : MonoBehaviour, IActiveInteract
+{
+    public Transform floatTextArea;
+    public string interactMessage = "[Collect Shard]";
+
+    //private FloatingTextController floatingText;
+    private bool _pickedUp = false;
+    public bool isInteractive => true;
+    void Start()
+    {
+    }
+
+    public void OnInteract(Player player)
+    {
+        if (!_pickedUp)
+        {
+            //increase player fragment count?
+            this.gameObject.SetActive(false);
+            //floatingText.DisableText(floatTextArea);
+            _pickedUp = true;
+        }
+    }
+
+    public void OnPlayerEnterInteractionRadius(Player player)
+    {
+    }
+
+    public void OnPlayerExitInteractionRadius(Player player)
+    {
+    }
+
+    public void OnSelected(Player player)
+    {
+    }
+
+    public void OnDeselected(Player player)
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
