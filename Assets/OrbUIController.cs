e5eabaa358182c9641c82fc061f6e7b707789b55
Rachel Haub <rachelhaub@gmail.com>
2020-05-01 18:30:05 -0700

    added orb ui animation

---
+++ Assets/OrbUIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbUIController : MonoBehaviour
+{
+    private Player _player = null;
+    private Player player => _player ?? Enforcements.GetSingleComponentInScene<Player>(this);
+
+    private Animation orbGetAnim;
+
+    private bool animPlayed = false;
+
+    private void Awake()
+    {
+        orbGetAnim = GetComponent<Animation>();
+    }
+
+    private void Update()
+    {
+        if(!animPlayed && player.fragments.fragmentCount == 7)
+        {
+            orbGetAnim.Play();
+            animPlayed = true;
+        }
+    }
+}
