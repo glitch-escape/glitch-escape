6edc0ef3cf2196ff727c52f7b5ce3e31cc7add9c
Rachel Haub <rachelhaub@gmail.com>
2020-05-01 19:46:01 -0700

    Linked portal to end card

--- Assets/GlitchEscape/Scripts/Mechanics/SceneLoading/Loader.cs
+++ Assets/GlitchEscape/Scripts/Mechanics/SceneLoading/Loader.cs
@@ -10,7 +10,7 @@ public static class Loader
     public enum Scene
     {
         MainMenu, Opening, Tutorial_01_Movement, Vertical_Main_Level, Vertical_Platforming_Level, Hub_Level,
-        CorruptionVisualTest, Tutorial_02_Advanced_Movement, Tutorial_03_Maze_Switch, Third_Level
+        CorruptionVisualTest, Tutorial_02_Advanced_Movement, Tutorial_03_Maze_Switch, Third_Level, SGDAEndCard
     }
 
     private static Action onLoadCallback;
