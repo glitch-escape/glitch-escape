d940e1370434e1c6adf9378fc84514fe8c746ac9
KrutoyChuvak <vkardash@ucsc.edu>
2020-05-06 17:54:01 -0700

    player is now saved across scenes
    player is loaded into do not destroy
    the player is moved to the position of the default player in the scene
    bug- camera is messed up right now, working on it

---
+++ Assets/GlitchEscape/Scripts/Mechanics/SceneLoading/DontDestroyPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyPlayer : MonoBehaviour
+{
+    [InjectComponent] public Player player;
+    //GameObject thePlayer = player.get
+    void Awake()
+    {
+        //FragmentInteraction[] allFragments = FindObjectsOfType<FragmentInteraction>();
+        DontDestroyOnLoad(FindObjectOfType<PlayerController>());
+    }
+}
