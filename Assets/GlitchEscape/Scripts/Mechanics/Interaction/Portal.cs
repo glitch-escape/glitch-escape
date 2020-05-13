c2dc6ff412be0ee6c7b49fc4e7ca44c3bb7123da
Seiji Emery <seijiemery@gmail.com>
2020-05-13 02:13:34 -0700

    fix for portal rotation speed

--- Assets/GlitchEscape/Scripts/Mechanics/Interaction/Portal.cs
+++ Assets/GlitchEscape/Scripts/Mechanics/Interaction/Portal.cs
@@ -10,7 +10,6 @@ public class Portal : AInteractiveObject
 {
     public string interactMessage = "[Step through the portal]";
     public Loader.Scene levelToLoad = Loader.Scene.MainMenu;
-    private float defaultPortalSpeed;
     public FloatRange focusedPortalSpeed = new FloatRange { maximum = 3f, minimum = 1f };
     [InjectComponent] public SphereCollider collider;
     [InjectComponent] public MeshRenderer renderer;
@@ -30,17 +29,7 @@ public class Portal : AInteractiveObject
     /// Is this portal currently focused by a nearby player?
     /// </summary>
     public bool focused { get; private set; } = false;
-
-    public float distanceToPlayer {
-        get {
-            var player = PlayerController.instance?.player;
-            return player != null ? Vector3.Distance(player.transform.position, transform.position) : Mathf.Infinity;
-        }
-    }
-
-    void Awake() {
-        defaultPortalSpeed = renderer.material.GetFloat(PORTAL_SHADER_SPEED);
-    }
+    
     public override void OnInteract(Player player) {
         if (levelToLoad != Loader.Scene.None) {
             Application.LoadLevel(levelToLoad.ToString());
@@ -48,22 +37,46 @@ public class Portal : AInteractiveObject
     }
     public override void OnFocusChanged(bool focused) {
         this.focused = focused;
-        if (!focused) renderer.material.SetFloat(PORTAL_SHADER_SPEED, defaultPortalSpeed);
     }
 
     private const string PORTAL_SHADER_SPEED = "Vector1_5C84EC34";
     private const string PORTAL_SHADER_COLOR = "Color_FFD634D4";
-    public float speed;
+    private const string PORTAL_SHADER_START_TIME = "StartTime";
+    private const float PORTAL_SHADER_PERIOD = 360f;
+    private Color portalShaderColor {
+        get => renderer.material.GetColor(PORTAL_SHADER_COLOR);
+        set => renderer.material.SetColor(PORTAL_SHADER_COLOR, value);
+    }
+    private float portalShaderSpeed {
+        get => renderer.material.GetFloat(PORTAL_SHADER_SPEED);
+        set {
+            var current = portalShaderSpeed;
+            if (value == current) return;
+            var start = renderer.material.GetFloat(PORTAL_SHADER_START_TIME);
+            var newStartWithOffsetForSpeed = (current / value) * (Time.time - start) - Time.time;
+            
+            renderer.material.SetFloat(PORTAL_SHADER_START_TIME, newStartWithOffsetForSpeed);
+            renderer.material.SetFloat(PORTAL_SHADER_SPEED, value);
+        }
+    }
     
+    private float defaultPortalSpeed;
+    void Awake() {
+        defaultPortalSpeed = portalShaderSpeed;
+    }
     private void Update() {
         if (focused) {
-            var player = PlayerController.instance.player;
+            var player = PlayerController.instance?.player;
             if (player == null || collider == null) return;
-            var distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
-            var normalizedDistance = Mathf.Clamp01(1f - distanceToPlayer / collider.radius);
+            
+            // get distance to player but only along the x / z plane
+            var toPlayer = player.transform.position - transform.position;
+            var distanceToPlayer = Mathf.Sqrt(toPlayer.x * toPlayer.x + toPlayer.z * toPlayer.z);
+            
+            // normalize against sphere collider radius + sample from min / max focusedPortalSpeed
+            var normalizedDistance = 1f - Mathf.Clamp01(distanceToPlayer / collider.radius * 1.5f);
             var portalSpeedWhenFocused = focusedPortalSpeed.Lerp(normalizedDistance);
-            speed = portalSpeedWhenFocused;
-            renderer.material.SetFloat(PORTAL_SHADER_SPEED, portalSpeedWhenFocused);
+            portalShaderSpeed = portalSpeedWhenFocused;
         }
     }
 }
