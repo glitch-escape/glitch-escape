a00d549155492881b0eda48e08a7329857fe8cce
Seiji Emery <seijiemery@gmail.com>
2020-05-13 13:21:46 -0700

    added editor script so you can switch mazes in the editor :)

---
+++ Assets/Editor/GlitchEscape/Scripts/MazeControllerEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+[CustomEditor(typeof(SceneMazeController))]
+public class MazeControllerEditor : Editor {
+    private SceneMazeController.ActiveMaze prevActiveMaze;
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        var mazeController = (SceneMazeController) target;
+        if (mazeController.activeMaze != prevActiveMaze) {
+            mazeController.SetActiveMaze(prevActiveMaze = mazeController.activeMaze);   
+        }
+    }
+}
