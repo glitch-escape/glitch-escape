b9367a809b7691df9df10cf22e224b923954b44b
KrutoyChuvak <vkardash@ucsc.edu>
2020-05-13 01:57:00 -0700

    all current levels updated with new maze controller

--- Assets/GlitchEscape/Scripts/Mechanics/MazeSwitching/SceneMazeController.cs
+++ Assets/GlitchEscape/Scripts/Mechanics/MazeSwitching/SceneMazeController.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Autowire))]
-//[InjectComponent] Maze allMazes;
+
 public class SceneMazeController : MonoBehaviour
 {
 
