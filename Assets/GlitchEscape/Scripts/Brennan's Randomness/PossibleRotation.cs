6cb08bef980087d313348f97e761b666b98b5f43
Brennan McCulloch <bmccullo@ucsc.edu>
2020-05-17 12:35:35 -0700

    OKAY MOM I DID IT
    Ok so basically, I made it so that you can rotate platforms around a central pivot point based on clicking a thing. I DID IT

---
+++ Assets/GlitchEscape/Scripts/Brennan's Randomness/PossibleRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PossibleRotation : MonoBehaviour
+{
+    bool rotate = false;
+    Vector3 rotatable = new Vector3(0f, 1f, 0f);
+
+    public void flickIt() { rotate = !rotate; }
+
+    private void Update()
+    {
+        if(rotate) {
+            this.gameObject.transform.Rotate(rotatable, 1, Space.Self);
+        }
+    }
+}
