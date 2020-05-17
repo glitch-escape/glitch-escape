e6bad2b73961575b120ef27f05b5c6db56045c96
Seiji Emery <seijiemery@gmail.com>
2020-05-17 14:32:56 -0700

    starting another pass on fragment visuals (this time using a particle system)
    unity VFX graph seemed overly complicated and did NOT give me the
    specific controls that I wanted, so an "easy" solution was to just write
    my own particle system as a monobehavior controlling a bunch of
    gameobjects (and if perf is ever an issue, could trivially convert
    this over to ECS)
    
    so far, got particle spawns and an interactive editor!
    (with random seeds saved + restored using Random.state so particles
    "stay" in the same spawn position when you're editing particle
    spawn properties)
    
    so far this is a KISS solution and works very well :)
    
    could probably consider adding 3d particles off platforms (and other
    effects) if / when I had time - and again, ECS offers a trivial route
    to parallelize this if performance ever becomes an issue

---
+++ Assets/Editor/GlitchEscape/Scripts/FragmentWorldVFXEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(FragmentWorldVFX))]
+public class FragmentWorldVFXEditor : Editor {
+    public override void OnInspectorGUI() {
+        var particleSystem = (FragmentWorldVFX) target;
+        var num = particleSystem.numParticles;
+        var radius = particleSystem.spawnRadius;
+        var height = particleSystem.spawnHeight;
+        var power = particleSystem.spawnHeightPowerBias;
+        var volume = particleSystem.spawnVolume;
+
+        if (GUILayout.Button("Spawn particles")) {
+            particleSystem.Respawn();
+        }
+        base.OnInspectorGUI();
+
+        if (particleSystem.initialized && (
+            num != particleSystem.numParticles ||
+            radius != particleSystem.numParticles ||
+            height != particleSystem.spawnHeight ||
+            power != particleSystem.spawnHeightPowerBias ||
+            volume != particleSystem.spawnVolume
+        )) {
+            particleSystem.RecalculateSpawnPositions();
+        }
+    }
+}
---
+++ Assets/GlitchEscape/Scripts/Mechanics/Fragments/FragmentWorldVFX/FragmentWorldVFX.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class FragmentWorldVFX : MonoBehaviour {
+    private FragmentWorldVFXParticle[] particles;
+    public FragmentWorldVFXParticle particle;
+    public uint numParticles;
+    
+    public float spawnRadius = 0.5f;
+    public float spawnHeight = 1.5f;
+    public float spawnHeightPowerBias = 1.5f;
+
+    public enum SpawnVolume { Sphere, Ellipsoid, DiamondEllipsoid }
+    public SpawnVolume spawnVolume;
+
+    public bool initialized => particles != null;
+
+    Vector3 GetRandomPosition() {
+        switch (spawnVolume) {
+            case SpawnVolume.Sphere:
+                return Random.insideUnitSphere * spawnRadius + transform.position;
+            case SpawnVolume.Ellipsoid:
+                var pos = Random.insideUnitSphere;
+                pos.y *= spawnHeight * 0.5f;
+                pos.x *= spawnRadius;
+                pos.z *= spawnRadius;
+                return pos + transform.position;
+            case SpawnVolume.DiamondEllipsoid:
+                var t = Mathf.Pow(Random.value, spawnHeightPowerBias);
+                var sign = Random.value < 0.5f ? -1f : 1f;
+                var y = sign * t * spawnHeight * 0.5f;
+                var xz = Random.insideUnitCircle * spawnRadius;
+                return new Vector3(xz.x, y, xz.y) + transform.position;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+    Quaternion GetRandomRotation() {
+        return Quaternion.AngleAxis(Random.Range(-180f, 180f), Vector3.up);
+    }
+    private Random.State spawnSeed;
+    public void Respawn() {
+        particles = GetComponentsInChildren<FragmentWorldVFXParticle>();
+        foreach (var particle in particles) {
+            DestroyImmediate(particle.gameObject);
+        }
+        spawnSeed = Random.state;
+        particles = new FragmentWorldVFXParticle[numParticles];
+        for (var i = 0; i < numParticles; ++i) {
+            particles[i] = Instantiate(this.particle, 
+                GetRandomPosition(),
+                GetRandomRotation(),
+                transform);
+        }
+    }
+    public void RecalculateSpawnPositions() {
+        var seed = Random.state;
+        Random.state = spawnSeed;
+        if (numParticles != (particles?.Length ?? 0)) {
+            Respawn();
+        } else {
+            foreach (var particle in particles) {
+                particle.transform.position = GetRandomPosition();
+                particle.transform.rotation = GetRandomRotation();
+            }
+        }
+        Random.state = seed;
+    }
+    
+    void Start() {
+        if (particles == null) {
+            Respawn();
+        }
+    }
+    void Update() {
+        foreach (var particle in particles) {
+            
+        }
+    }
+}
