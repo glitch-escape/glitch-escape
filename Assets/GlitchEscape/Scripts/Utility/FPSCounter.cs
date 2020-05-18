8d22ba85374d730c4f253be364810220607c4125
Seiji Emery <seijiemery@gmail.com>
2020-05-18 16:12:09 -0700

    added framerate counter

---
+++ Assets/GlitchEscape/Scripts/Utility/FPSCounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class FPSCounter : MonoBehaviour {
+    [InjectComponent] public TextMeshProUGUI text;
+    private float fps = 60f;
+    void Update() {
+        var step = 0.1f;
+        fps = fps * (1f - step) + step / Time.deltaTime;
+        text.text = "FPS: " + Mathf.Round(fps);
+    }
+}
