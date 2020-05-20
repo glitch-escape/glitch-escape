8ff7c2aa1c67181038eb3b58451557c16a54c512
Brennan McCulloch <bmccullo@ucsc.edu>
2020-05-20 14:26:07 -0700

    everything's ready for ben.

--- Assets/GlitchEscape/Scripts/Brennan's Randomness/PossibleRotation.cs
+++ Assets/GlitchEscape/Scripts/Brennan's Randomness/PossibleRotation.cs
@@ -6,13 +6,25 @@ public class PossibleRotation : MonoBehaviour
 {
     bool rotate = false;
     Vector3 rotatable = new Vector3(0f, 1f, 0f);
+    int degrees = 0;
+    int howMuchToRotate = 2;
 
-    public void flickIt() { rotate = !rotate; }
+    public void flickIt() { rotate = !rotate; degrees = 0; }
 
     private void Update()
     {
-        if(rotate) {
-            this.gameObject.transform.Rotate(rotatable, 2, Space.Self);
+        if(rotate && degrees != 45) {
+            this.gameObject.transform.Rotate(rotatable, howMuchToRotate, Space.Self);
+            degrees += howMuchToRotate;
+            if(degrees >= 45)
+            {
+                rotate = false;
+            }
+        }
+
+        if(!rotate && degrees >= 45)
+        {
+            degrees = 0;
         }
     }
 }
