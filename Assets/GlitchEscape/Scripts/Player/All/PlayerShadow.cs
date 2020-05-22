189fef4bea705acd278e1de9334a5b715c3f9233
Seiji Emery <seijiemery@gmail.com>
2020-05-22 14:31:44 -0700

    fixed raycasts and added a static utility class to centralize layer masks

--- Assets/GlitchEscape/Scripts/Player/All/PlayerShadow.cs
+++ Assets/GlitchEscape/Scripts/Player/All/PlayerShadow.cs
@@ -8,6 +8,9 @@ public class PlayerShadow : MonoBehaviour
     public GameObject landingIndicator;
     private Vector3 displayOffset;
     
+    // hardcoded max raycast distance (should be sufficient, hopefully)
+    private const float MAX_RAYCAST_DISTANCE = 50f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -42,12 +45,13 @@ public class PlayerShadow : MonoBehaviour
     }
 
     private RaycastHit surfaceHit;
-    private bool GetFloorRaycast()
-    {
+    private bool GetFloorRaycast() {
         return Physics.Raycast(
             player.transform.position,
             Vector3.down,
             out surfaceHit,
-            Mathf.Infinity);
+            MAX_RAYCAST_DISTANCE,
+            LayerMasks.FloorGeometry,
+            QueryTriggerInteraction.Ignore);
     }
 }
---
+++ Assets/GlitchEscape/Scripts/Utility/LayerMasks.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Utility class for getting glitch escape physics layers. Added this so that if we ever change how we're using layers
+/// all layer masks can be put (and updated) in one location.
+/// </summary>
+public class LayerMasks {
+    public static int FloorGeometry => LayerMask.GetMask("Default", "BlueMaze", "PinkMaze");
+    public static int Walls => LayerMask.GetMask("Wall");
+    public static int FloorAndWalls => LayerMask.GetMask("Default", "Wall", "BlueMaze", "PinkMaze");
+}
