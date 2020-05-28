7eeed00e06a072b25f00e8fdf272d70766dfdfb4
Seiji Emery <seijiemery@gmail.com>
2020-05-28 15:49:11 -0700

    cleanup fragment code + setup stubs for future vfx for pick up effects + focus change effects

--- Assets/GlitchEscape/Scripts/Mechanics/Fragments/FragmentPickup.cs
+++ Assets/GlitchEscape/Scripts/Mechanics/Fragments/FragmentPickup.cs
@@ -4,27 +4,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FragmentPickup : PersistentInteractiveObject<FragmentPickup.State> {
+    public Transform floatTextArea;
+    public string interactMessage = "[Collect Shard]";
+    //private FloatingTextController floatingText;
+    public GameObject fragmentUI;
+
+    /// stores persistent state for this object
     public struct State {
         public bool collected; // = false
     }
+    
+    /// called if / when state is restored
+    /// called by <see cref="PersistentInteractiveObject{TState}"/>, in OnEnable() if / when state is restored
+    /// note that state is saved in OnDisable(), which at minimum will be called when another scene is loaded
+    /// (and in this case will be called after gameObject.SetActive())
     protected override void OnStateRestored() {
+        // directly set state + set active
         if (state.collected) {
             gameObject.SetActive(false);
         }
     }
-
-    public Transform floatTextArea;
-    public string interactMessage = "[Collect Shard]";
-
-    //private FloatingTextController floatingText;
-    public bool fragmentIsPickedUp = false;
-    public GameObject fragmentUI;
-
-    public override void OnFocusChanged(bool focused) {}
-
+    
+    /// <summary>
+    ///  is this fragment currently picked up?
+    /// (wraps State.collected + responsible 
+    /// </summary>
+    public bool collected {
+        get => state.collected;
+        set {
+            state.collected = value;
+            if (collected) {
+                OnFragmentPickedUp();
+            }
+        }
+    }
+    
+    /// called when player picks up fragments
+    /// interact behavior set in inspector, implemented in <see cref="AInteractiveObject"/>
     public override void OnInteract(Player player) {
-        state.collected = true;
-        gameObject.SetActive(false);
+        collected = true;
         player.GetComponent<FragmentPickupManager>()?.PickUpFragment(this);
     }
+
+    /// use to implement fragment pick up transitions / effects, etc
+    /// called if / when this.collected is set to true
+    /// TODO: use this to implement visual fragment pick up effect / animation / etc
+    private void OnFragmentPickedUp() {
+        gameObject.SetActive(false);
+    }
+    
+    /// called when player is nearby / enters some interaction radius (if focused behavior is activated and set
+    /// to activate on trigger; if so, must use a separate collider for interact)
+    /// TODO: use this to implement fragment highlighting when player gets close
+    public override void OnFocusChanged(bool focused) {}
 }
