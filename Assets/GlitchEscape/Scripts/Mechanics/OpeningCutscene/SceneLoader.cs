59f6048c40eb4b5ad73b685af70dc8c2b5ab0c78
Seiji Emery <seijiemery@gmail.com>
2020-05-27 18:28:49 -0700

    add scripts + event to play next scene

---
+++ Assets/GlitchEscape/Scripts/Mechanics/OpeningCutscene/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    public void LoadNextScene() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadMainMenu() {
+        Loader.Load(Loader.Scene.MainMenu);   
+    }
+    public void LoadFirstTutorial() {
+        Loader.Load(Loader.Scene.Tutorial_01_Movement);
+    }
+    public void LoadHub() {
+        Loader.Load(Loader.Scene.Hub_Level);
+    }
+    public void LoadExplorationLevel() {
+        Loader.Load(Loader.Scene.Vertical_Main_Level);
+    }
+    public void LoadPlatformingLevel() {
+        Loader.Load(Loader.Scene.Vertical_Platforming_Level);
+    }
+    public void LoadThirdLevel() {
+        Loader.Load(Loader.Scene.Third_Level);
+    }
+}
