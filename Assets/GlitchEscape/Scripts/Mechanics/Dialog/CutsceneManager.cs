9c9dc6c0745a199fcab62fe429f8ef759961d1ea
Hardwin Bui <buihardwin@yahoo.com>
2020-06-02 03:13:16 -0700

    Linked cutscenes to game
    There's a bug caused by enabling/disabling the playerCam rig that makes loading look weird

--- Assets/GlitchEscape/Scripts/Mechanics/Dialog/CutsceneManager.cs
+++ Assets/GlitchEscape/Scripts/Mechanics/Dialog/CutsceneManager.cs
@@ -5,26 +5,47 @@ using UnityEngine;
 /// <summary>
 /// This script should only be place in a scene solely dedicated for cutscenes.
 /// - Prevents an instance of the PlayCamRig from being active while the cutscene is playing
-/// - Allows timeline to decide when the cutscene ends
+/// - Allows timeline to decide when the cutscene ends and load different levels
 /// </summary>
 public class CutsceneManager : MonoBehaviour {
 
-    private PlayerController player;
+    private PlayerController playerController;
+    public Loader.Scene levelToLoad = Loader.Scene.MainMenu;
+    public Loader.Scene endCutsceneToLoad = Loader.Scene.MainMenu;
+    public Virtue[] endingVirtuesRequired;
 
     void Awake() {
-        player = FindObjectOfType<PlayerController>();
-        if(player) { player.gameObject.SetActive(false); }
+        playerController = FindObjectOfType<PlayerController>();
+        if(playerController) playerController.gameObject.SetActive(false); 
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    public void EndScene() {
+        if(playerController) { 
+            playerController.gameObject.SetActive(true); 
+            bool hasAllVirtues = true;
+            for(int i = 0; i < endingVirtuesRequired.Length; i++) {
+                if(!playerController.player.fragments.IsVirtueCompleted(endingVirtuesRequired[i])) {
+                    hasAllVirtues = false;
+                    break;
+                }
+            }
+            if(endCutsceneToLoad != Loader.Scene.None && hasAllVirtues) {
+                Application.LoadLevel(endCutsceneToLoad.ToString());
+                return;
+            }
+        }
         
+        if (levelToLoad != Loader.Scene.None) {
+            Application.LoadLevel(levelToLoad.ToString());
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        /*
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            EndScene();
+        }
+        */
     }
 }
