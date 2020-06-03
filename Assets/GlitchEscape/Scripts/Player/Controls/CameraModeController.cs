4d295a7a2f7227d814673744c99d53cdcdd62add
KrutoyChuvak <vkardash@ucsc.edu>
2020-06-03 03:01:53 -0700

    moved some code and updated player rig

--- Assets/GlitchEscape/Scripts/Player/Controls/CameraModeController.cs
+++ Assets/GlitchEscape/Scripts/Player/Controls/CameraModeController.cs
@@ -64,7 +64,6 @@ public class CameraModeController : MonoBehaviour
             && currentCamMode != CameraMode.HardFollow)
         {
             ResetCamera();
-            
         }
 
         
