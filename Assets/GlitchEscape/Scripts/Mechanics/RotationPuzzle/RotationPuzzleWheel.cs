6c61802aeb64bd2380947c5a04dbd56242156630
Seiji Emery <seijiemery@gmail.com>
2020-06-03 18:25:31 -0700

    added puzzle test scene + new puzzle controller script
    (didn't have this previously, ergo no way to add feedback, button
    prompts, etc w/out duct taping even more stuff on top of the
    unity-events-based interaction script (which I probably shouldn't have
    added, b/c you were supposed to write scripts like this + derive from
    AInteractivObject...)

---
+++ Assets/GlitchEscape/Scripts/Mechanics/RotationPuzzle/RotationPuzzleWheel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationPuzzleWheel : AInteractiveObject {
+    private bool focused = false;
+    private Player player;
+    public PossibleRotation target;
+    void OnEnable() {
+        player = FindObjectOfType<Player>();
+    }
+    private void OnDisable() {
+        player = null;
+    }
+    public override void OnInteract(Player player) {
+        target?.flickIt();
+    }
+    public override void OnFocusChanged(bool focused) {
+        this.focused = focused;
+    }
+}
