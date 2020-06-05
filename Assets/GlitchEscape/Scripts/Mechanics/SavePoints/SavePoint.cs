f8a7c23142224a225b9552e8c7586e2160c99f00
Seiji Emery <seijiemery@gmail.com>
2020-06-05 00:31:21 -0700

    added save points on puzzle A

--- Assets/GlitchEscape/Scripts/Mechanics/SavePoints/SavePoint.cs
+++ Assets/GlitchEscape/Scripts/Mechanics/SavePoints/SavePoint.cs
@@ -3,8 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SavePoint : MonoBehaviour
-{
+public class SavePoint : MonoBehaviour {
+    [Tooltip("optional, if not set save point origin will be set to this object itself")]
+    public Transform savePointTarget;
     private void OnTriggerEnter(Collider other) {
         other.GetComponent<Player>()?.spawn.SetSpawnPosition(this);
     }
