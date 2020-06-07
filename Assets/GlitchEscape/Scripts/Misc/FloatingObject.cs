726100dba5da6415fc23142a9dd05d3f302e4969
Seiji Emery <seijiemery@gmail.com>
2020-06-06 22:43:33 -0700

    added a basic script to make tank NPCs (and other objects) float

---
+++ Assets/GlitchEscape/Scripts/Misc/FloatingObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[ExecuteInEditMode]
+public class FloatingObject : MonoBehaviour {
+    private Vector3 centerPosition;
+    private float startTime;
+
+    private void OnEnable() {
+        Setup();
+    }
+    public void Setup() {
+        centerPosition = transform.position; 
+        startTime = Time.time;
+        if (randomizeFloatStartTimes) {
+            startTime += Random.Range(-cycleDuration, +cycleDuration);
+        }
+    }
+    
+    public float cycleDuration = 1f;
+    public float floatOffsetDistance = 0.2f;
+    public bool randomizeFloatStartTimes;
+    
+    void Update() {
+        var t = (Time.time - startTime) / cycleDuration;
+        var cycle = Mathf.Sin(t * Mathf.PI * 2f);
+        transform.position = centerPosition + Vector3.up * cycle * floatOffsetDistance;
+    }
+}
