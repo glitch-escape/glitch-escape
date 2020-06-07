885941508a7904508e0bd4218036101e12bade83
Seiji Emery <seijiemery@gmail.com>
2020-06-06 23:49:24 -0700

    added pseudo-random rotations

--- Assets/GlitchEscape/Scripts/Misc/FloatingObject.cs
+++ Assets/GlitchEscape/Scripts/Misc/FloatingObject.cs
@@ -7,7 +7,9 @@ using Random = UnityEngine.Random;
 [ExecuteInEditMode]
 public class FloatingObject : MonoBehaviour {
     private Vector3 centerPosition;
+    private Quaternion initialRotation;
     private float startTime;
+    private float rotationTimeOffset;
 
     private void OnEnable() {
         Setup();
@@ -18,15 +20,26 @@ public class FloatingObject : MonoBehaviour {
         if (randomizeFloatStartTimes) {
             startTime += Random.Range(-cycleDuration, +cycleDuration);
         }
+        initialRotation = transform.rotation;
+        rotationTimeOffset = Random.Range(-rotationCycleDuration, +rotationCycleDuration);
     }
     
     public float cycleDuration = 10f;
     public float floatOffsetDistance = 0.05f;
     public bool randomizeFloatStartTimes;
+    public bool applyRotation = true;
+    public float rotationCycleDuration = 4f;
+    public float rotationDegrees = 15f;
     
     void Update() {
         var t = (Time.time - startTime) / cycleDuration;
         var cycle = Mathf.Sin(t * Mathf.PI * 2f);
         transform.position = centerPosition + Vector3.up * cycle * floatOffsetDistance;
+
+        if (applyRotation) {
+            var t2 = (Time.time - startTime + rotationTimeOffset) / rotationCycleDuration;
+            var angle = Mathf.Sin(t2 * Mathf.PI * 2f) * rotationDegrees;
+            transform.rotation = initialRotation * Quaternion.AngleAxis(angle, Vector3.up);
+        }
     }
 }
