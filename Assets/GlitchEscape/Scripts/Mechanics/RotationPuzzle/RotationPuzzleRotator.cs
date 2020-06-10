4b725998dd844a454d274be11d46a75e14acd4f3
Seiji Emery <seijiemery@gmail.com>
2020-06-09 23:56:25 -0700

    moved out rotation puzzle config (fixed bug where you couldn't edit the player puzzle config in the unity editor)

--- Assets/GlitchEscape/Scripts/Mechanics/RotationPuzzle/RotationPuzzleRotator.cs
+++ Assets/GlitchEscape/Scripts/Mechanics/RotationPuzzle/RotationPuzzleRotator.cs
@@ -5,16 +5,6 @@ using UnityEngine;
 using UnityEditor;
 #endif
 
-[CreateAssetMenu(fileName="GameConfigs", menuName = "Rotation Puzzle Config", order = 1)]
-public class RotationPuzzleConfig : ScriptableObject {
-    public float fullRotationAngle = 45f;
-    public AudioClip fullRotationAudioClip;
-    public bool useAudioClipDuration = true;
-    public float fallbackDuration = 3f;
-    public bool stopAudioAtEnd = true;
-}
-
-
 public class RotationPuzzleRotator : MonoBehaviour {
     private RotationPuzzleConfig _config;
     public RotationPuzzleConfig config => Player.instance?.config.rotationPuzzleConfig;
